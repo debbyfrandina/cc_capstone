1ae0fefa67f8b1279fa6e646bd380124bf92e4b1
debbyfrandina <74141415+debbyfrandina@users.noreply.github.com>
2023-05-31 17:00:35 +0700

    upload

---
+++ app/Http/Controllers/AuthController.php
@@ -0,0 +1,42 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
+use App\Models\User;
+
+class AuthController extends Controller
+{
+    public function register(Request $request)
+    {
+        $validatedData = $request->validate([
+            'username' => 'required|max:255',
+            'name' => 'required|max:255',
+            'email' => 'required|email|unique:users',
+            'password' => 'required|min:6',
+        ]);
+
+        $user = User::create($validatedData);
+
+        $accessToken = $user->createToken('authToken')->accessToken;
+
+        return response(['user' => $user, 'access_token' => $accessToken]);
+    }
+
+    public function login(Request $request)
+    {
+        $loginData = $request->validate([
+            'email' => 'required|email',
+            'password' => 'required',
+        ]);
+
+        if (!Auth::attempt($loginData)) {
+            return response(['message' => 'Invalid credentials']);
+        }
+
+        $accessToken = Auth::user()->createToken('authToken')->accessToken;
+
+        return response(['user' => Auth::user(), 'access_token' => $accessToken]);
+    }
+}
